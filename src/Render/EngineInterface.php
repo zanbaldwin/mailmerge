c4c224db12fd579685951d593f626a13aa17b251
Zander Baldwin <mynameiszanders@gmail.com>
2014-10-17 19:00:33 +0100

    Update Render Engine Interface

--- src/Render/EngineInterface.php
+++ src/Render/EngineInterface.php
@@ -4,4 +4,33 @@
 
     interface EngineInterface
     {
+
+        /**
+         * Constructor
+         *
+         * @access public
+         * @param MailMerge\Render\Parser\ParserInterface $parser
+         * @param MailMerge\Render\Generator\GeneratorInterface $generator
+         * @return void
+         */
+        public function __construct(ParserInterface $parser, GeneratorInterface $generators);
+
+        /**
+         * Set: Template Parser
+         *
+         * @access public
+         * @param MailMerge\Render\Parser\ParserInterface $parser
+         * @return self
+         */
+        public function setParser(ParserInterface $parser);
+
+        /**
+         * Add: Document Generator
+         *
+         * @access public
+         * @param MailMerge\Render\Generator\GeneratorInterface $generator
+         * @return self
+         */
+        public function addGenerator(GeneratorInterface $generator);
+
     }
