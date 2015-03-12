cefd4565dea8025c8a9854903f8c238e54f1936b
Zander Baldwin <mynameiszanders@gmail.com>
2015-03-12 13:33:57 +0000

    Update Autoloader

--- src/MailMerge.php
+++ src/MailMerge.php
@@ -16,16 +16,15 @@
          */
         public static function load($class)
         {
-            if(substr($class, 0, 10) !== 'MailMerge\\') {
+            $psr4 = 'MailMerge\\';
+            if(substr($class, 0, strlen($psr4)) !== $psr4) {
                 return;
             }
-            $library_root = realpath(__DIR__ . DIRECTORY_SEPARATOR);
-            $file = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 10)) . '.php';
-            $file = realpath($library_root . DIRECTORY_SEPARATOR . $file);
-            if(substr($file, 0, strlen($library_root)) == $library_root) {
-                if(is_readable($file)) {
-                    include $file;
-                }
+
+            $root = rtrim(realpath(__DIR__ . '/../src'), '/') . '/';
+            $file = str_replace('\\', '/', substr($class, strlen($psr4))) . '.php';
+            if(is_readable($filepath = $root . $file)) {
+                include $filepath;
             }
         }
 
@@ -45,5 +44,5 @@
 
     }
 
-    // Add the PHPerian autoloader method to the PHP's SPL Autoloader.
+    // Add the MailMerge autoloader method to PHP's SPL Autoloader.
     MailMerge::registerAutoloader();
